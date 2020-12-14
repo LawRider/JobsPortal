ae3460964f27881d50871aa5530be58e90a9ef4c
Oleksandr <9040602+LawRider@users.noreply.github.com>
2020-12-14 02:58:52 +0200

    Update admin.py

--- hyperjob/vacancy/admin.py
+++ hyperjob/vacancy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Vacancy
 
-# Register your models here.
+
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Vacancy, VacancyAdmin)
